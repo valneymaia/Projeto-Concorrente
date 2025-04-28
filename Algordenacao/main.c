0d7a355fcc94daa3d36b67c11b7e4f012d5a90bb
VALNEY MAIA <93133793+valneymaia@users.noreply.github.com>
2025-04-28 08:38:45 -0300

    Update main.c

--- Algordenacao/main.c
+++ Algordenacao/main.c
@@ -1,9 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <time.h>
+#include <omp.h>  // Incluir OpenMP para paralelização
 
 #define THRESHOLD 1000  // Limite mínimo para paralelismo
+
+
+void selectionSortSerial(int arr[], int n) {
+    int i, j, minIdx, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        minIdx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+
+        temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+// Versão paralela do Selection Sort
+void selectionSortParalelo(int arr[], int n) {
+    int i, j, minIdx, maxIdx, temp;
+    int metade = n / 2;
+
+    #pragma omp parallel
+    {
+        for (i = 0; i < metade; i++) {
+            minIdx = i;
+            maxIdx = n - i - 1;
+
+            #pragma omp single nowait
+            {
+
+                #pragma omp task shared(minIdx)
+                //#pragma omp task
+                {
+                    for (j = i + 1; j < n - i; j++) {
+                        if (arr[j] < arr[minIdx]) {
+                            minIdx = j;
+                        }
+                    }
+                }
+
+
+                #pragma omp task shared(maxIdx)
+                //#pragma omp task
+                {
+                    for (j = i; j < n - i - 1; j++) {
+                        if (arr[j] > arr[maxIdx]) {
+                            maxIdx = j;
+                        }
+                    }
+                }
+
+                #pragma omp taskwait
+
+                if (minIdx != i) {
+                    temp = arr[i];
+                    arr[i] = arr[minIdx];
+                    arr[minIdx] = temp;
+                }
+
+                if (maxIdx == i) {
+                    maxIdx = minIdx;
+                }
+
+                if (maxIdx != n - i - 1) {
+                    temp = arr[n - i - 1];
+                    arr[n - i - 1] = arr[maxIdx];
+                    arr[maxIdx] = temp;
+                }
+            }
+        }
+    }
+}
 
 void swap(int *a, int *b) {
     int temp = *a;
@@ -59,18 +134,58 @@ void quicksort_parallel(int arr[], int low, int high) {
     }
 }
 
-void print_array(int arr[], int size) {
+// Função para imprimir o vetor
+void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
+void gerarVetorAleatorio(int arr[], int n, int max_valor) {
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % (max_valor + 1);
+    }
+}
+
 int main() {
+    int n = 100000;
+    int max_valor = 100000;
+
+    int arrSerial[n];
+    int arrParalelo[n];
+
+    // Gera o vetor aleatório
+    gerarVetorAleatorio(arrSerial, n, max_valor);
+    for (int i = 0; i < n; i++) {
+        arrParalelo[i] = arrSerial[i];
+    }
+    //printArray(arrParalelo, n);
+
+    double inicio,fim;
+    inicio = omp_get_wtime();
+    selectionSortSerial(arrSerial, n);
+    fim = omp_get_wtime();
+    double t_serial = fim-inicio;
+    printf("Execucao serial: %f\n",t_serial);
+
+   // printArray(arrSerial, n);
+
+    inicio = omp_get_wtime();
+    selectionSortParalelo(arrParalelo, n);
+	fim = omp_get_wtime();
+    double t_paralelo = fim - inicio;
+	printf("Execucao paralela: %f\n",t_paralelo);
+
+    //printArray(arrParalelo, n);
+
+
     int a = 1000000;  // Teste com tamanho grande para notar diferença
     int *vet_seq = (int *)malloc(sizeof(int) * a);
     int *vet_par = (int *)malloc(sizeof(int) * a);
-    int n = a;
+    int m = a;
 
     // Verifica se a alocação de memória foi bem-sucedida
     if (vet_seq == NULL || vet_par == NULL) {
@@ -87,15 +202,16 @@ int main() {
         vet_par[i] = num; // Cópia para manter os mesmos valores nos dois testes
     }
 
-    // Execução Sequencial
+
+      // Execução Sequencial
     double start_seq = omp_get_wtime();
-    quicksort_sequential(vet_seq, 0, n - 1);
+    quicksort_sequential(vet_seq, 0, m - 1);
     double end_seq = omp_get_wtime();
     printf("Tempo de execução (Sequencial): %.6f segundos\n", end_seq - start_seq);
 
     // Execução Paralela
     double start_par = omp_get_wtime();
-    quicksort_parallel(vet_par, 0, n - 1);
+    quicksort_parallel(vet_par, 0, m - 1);
     double end_par = omp_get_wtime();
     printf("Tempo de execução (Paralelo): %.6f segundos\n", end_par - start_par);
 
@@ -103,5 +219,11 @@ int main() {
     free(vet_seq);
     free(vet_par);
 
+
     return 0;
 }
+
+
+
+
+
